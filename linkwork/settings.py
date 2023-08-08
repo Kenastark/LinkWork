67fe047db50977d893a60a7dfcf4c98da865071b
kenastark <ikennaudeani@gmail.com>
2023-08-08 23:02:23 +0100

    modified DATABASES settings to mysql

--- linkwork/settings.py
+++ linkwork/settings.py
@@ -90,8 +90,10 @@ WSGI_APPLICATION = 'linkwork.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'linkwork',
+        'HOST': 'root',
+        'PASSWORD': 'kena199892'
     }
 }
 
